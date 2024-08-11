13c57e9ab32bae052933060a75f648dcd6ea13e3
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-08-11 13:55:47 +0300

    Add project files.

---
+++ MyBlog.DataAccessLayer/EntityFramework/EFCategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using MyBlog.DataAccessLayer.Abstract;
+using MyBlog.DataAccessLayer.Repositories;
+using MyBlog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.DataAccessLayer.EntityFramework
+{
+    //efcategorydal sadece generic repositorydeki metotlarla yetinmeyip ileride sadece category ye ilişkin metotlar isteyebilir o yüzden sadece category ye özel metotlar ICategorydal da olduğu için virgül koyup oradan da miras aldık
+    //bir sınıf bir interface den miras alırken implement yapmak gerekiyor ama aşağıda implement yapmamızın sebebi genericrepository de olmuş olması 
+    public class EFCategoryDal : GenericRepository<Category>, ICategoryDal
+    {
+    }
+}
---
+++ MyBlog.PresentationLayer/Program.cs
@@ -0,0 +1,37 @@
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.BusinessLayer.Concrete;
+using MyBlog.DataAccessLayer.Abstract;
+using MyBlog.DataAccessLayer.EntityFramework;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//dependency injection da dýþardan enjeksiyon kýsmý burada yapýlýr
+//ICategoryService gördüðünde EFCategoryDal çaðýr
+builder.Services.AddScoped<ICategoryService, CategoryManager>();
+builder.Services.AddScoped<ICategoryDal, EFCategoryDal>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
