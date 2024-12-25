ad986e39cc38b55a218ea93332ad58aac1a8d945
Ezelhan Dikmenlioğlu <20060394@stu.omu.edu.tr>
2024-12-25 09:50:43 +0300

    ...

--- MyBlog.PresentationLayer/Program.cs
+++ MyBlog.PresentationLayer/Program.cs
@@ -48,4 +48,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.Run();
